6441adebf39c79ddcca5f6a3c3627e73cb13a876
rvalencia <rva.valencia@gmail.com>
2025-04-08 18:49:08 -0400

    feat: add ppt exercises

---
+++ guiappt/ejercicio01.py
@@ -0,0 +1,8 @@
+num1 = float(input('Ingrese un numero decimal:\n'))
+num2 = float(input('Ingrese otro numero:\n'))
+num3 = float(input('Ingrese un tercer numero:\n'))
+num4 = float(input('Ingrese un ultimo numero: '))
+
+suma = num1 + num2 + num3 + num4 
+
+print(f'El resultado de la suma es: {suma}')
