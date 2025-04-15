5e029c9248aa49872ac622b814c612aad4dcf387
rvalencia <rva.valencia@gmail.com>
2025-04-14 20:34:41 -0400

    feat: add return functions

--- ejercicio16.py
+++ ejercicio16.py
@@ -1,1 +1,15 @@
 # Funciones CON RETORNO
+# NO RETORNA
+def suma(a, b):
+    print(a + b)
+    
+resultado = suma(3, 8) # 11
+print(resultado) # None : La ausencia de valor
+
+# Funcion con retorno
+def suma(a, b):
+    return a + b # Esto devuelve el valor de la suma al contexto de la invocacion
+    print('Hola mundo') # Esto no se ejecuta
+
+resultado = suma(3, 8) # 11
+print(resultado)
